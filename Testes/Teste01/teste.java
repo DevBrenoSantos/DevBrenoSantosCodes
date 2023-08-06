a31f8a51c27b68175a690bad228c07541c8602bc
DevBrenoSantos <contato.brenoamaral.s@gmail.com>
2023-08-06 13:32:43 -0300

    Atualizações

---
+++ Testes/Teste01/teste.java
@@ -0,0 +1,20 @@
+package Testes.Teste01;
+import java.util.Scanner;
+public class teste {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int tempo = 1, limite;
+        System.out.println("Insira o tempo em segundos: ");
+        limite = console.nextInt();
+        for (int aux=0; aux<=(limite - 1); aux++) {
+            System.out.println("Tempo: "+tempo+" segundos");
+            tempo++;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Tempo Esgotado!");
+    }
+}
